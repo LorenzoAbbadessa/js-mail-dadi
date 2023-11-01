464ceef6c94cf08fc29c7ce28bee7222246a1a04
lorenzo <lor.abbadessa@gmail.com>
2023-11-01 16:07:03 +0100

    completa esercizio

---
+++ js.js
@@ -0,0 +1,60 @@
+/*const email = prompt("inserisci la tua mail")
+const num = ("1@gmail.com", "2@gmail.com", "3@gmail.com", "4@gmail.com")
+
+
+if ( email.indexOf( num ) ){ 
+    console.log ("ok")
+}else {
+    console.log("notok")
+}*/
+
+
+/*var inserimento = prompt("inserisci la tua mail")
+var email = ("1@gmail.com", "2@gmail.com", "3@gmail.com")
+
+
+if(email.indexOf("1@gmail.com", "2@gmail.com", "3@gmail.com") !== -1)  
+{  
+        alert("Yes, the value exists!")  
+}   
+else  
+{  
+        alert("No, the value is absent.")  
+}  */
+/*const email = prompt("inserisci la tua mail")
+const army=["Marcos", "DeltaForce", "Seals", "SWAT", "HeadHunters"];  
+  
+if(army.indexOf("Marcos") !== -1)  
+{  
+        alert("Yes, the value exists!")  
+}   
+else  
+{  
+        alert("No, the value is absent.")  
+}  */
+
+
+/*let letters = ["a", "e", "i", "o", "u"];
+
+let selection = prompt ("enter a letter")
+
+
+if (letters.includes("selection") == "true") {
+alert ('yes');
+} else {
+    alert ('no');
+};*/
+
+
+
+var mail = ["1@gmail.com", "2@gmail.com", "3@gmail.com", "4@gmail.com", "5@gmail.com"];
+
+var selection = prompt ("inserisci la tua mail")
+
+
+if (mail.includes(selection)) {
+    console.log('accesso consentito');
+} else {
+    console.log('accesso negato!');
+};
+    
